8083e5f10f54e5b80872d53e578a21d297c3b990
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-06-15 13:03:25 -0300

    Reproducción del audio

--- index.js
+++ index.js
@@ -1,26 +1,57 @@
 import { HfInference } from '@huggingface/inference';
 import dotenv from 'dotenv';
 import fs from 'fs';
+import Speaker from 'speaker';
+import ffmpeg from 'fluent-ffmpeg';
 
 dotenv.config();
 
-async function saveTextToSpeech() {
-  try {
-    const hf = new HfInference(process.env.HF_ACCESS_TOKEN);
-    const text = 'Hola, como estas?';
+async function generateAudioFromText(texto) {
+  const hf = new HfInference(process.env.HF_ACCESS_TOKEN);
+  const respuesta = await hf.textToSpeech({
+    inputs: texto,
+    model: 'facebook/mms-tts-spa'
+  });
+  return Buffer.from(await respuesta.arrayBuffer());
+}
 
-    const response = await hf.textToSpeech({
-      inputs: text,
-      model: 'facebook/mms-tts-spa'
+function saveAudio(buffer, rutaArchivo) {
+  fs.writeFileSync(rutaArchivo, buffer);
+}
+
+function playAudio(rutaArchivo) {
+  if (fs.existsSync(rutaArchivo)) {
+    const stream = fs.createReadStream(rutaArchivo);
+    const speaker = new Speaker({
+      channels: 2, // Número de canales de audio
+      bitDepth: 16, // Determina la calidad del audio
+      sampleRate: 9500 // Determina la velocidad de muestreo del audio
     });
 
-    console.log('Respuesta de la API de Hugging Face: ', response);
+    const command = ffmpeg(stream)
+      .toFormat('s16le')
+      .pipe(speaker);
 
-    const buffer = await response.arrayBuffer();
-    const bufferNode = Buffer.from(buffer);
-    fs.writeFileSync('output.flac', bufferNode);
+    command.on('error', (err) => {
+      console.error('Error al reproducir el archivo:', err);
+    });
 
-    console.log('\nArchivo de audio guardado como output.flac');
+    speaker.on('close', () => {
+      console.log('Reproducción finalizada.');
+    });
+  } else {
+    console.error('El archivo de audio no existe o no se creó correctamente.');
+  }
+}
+
+async function saveTextToSpeech() {
+  try {
+    const texto = 'Hola, como estas? Mi nombre es Alejandro y tengo veintitrés años.';
+    const bufferAudio = await generateAudioFromText(texto);
+    const rutaTemporal = 'output_temp.flac';
+    saveAudio(bufferAudio, rutaTemporal);
+
+    playAudio(rutaTemporal);
   } catch (error) {
     console.error('Error al generar el texto a voz:', error);
   }
