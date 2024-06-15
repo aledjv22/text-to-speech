e4ce48ab54f614b5e96d7ea7f361dbd778f5fd08
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-06-15 10:08:50 -0300

    Captura de errores

--- index.js
+++ index.js
@@ -4,23 +4,26 @@ import fs from 'fs';
 
 dotenv.config();
 
-
 async function saveTextToSpeech() {
-  const hf = new HfInference(process.env.HF_ACCESS_TOKEN);
-  const text = 'Hola, como estas?';
+  try {
+    const hf = new HfInference(process.env.HF_ACCESS_TOKEN);
+    const text = 'Hola, como estas?';
 
-  const response = await hf.textToSpeech({
-    inputs: text,
-    model: 'facebook/mms-tts-spa'
-  });
+    const response = await hf.textToSpeech({
+      inputs: text,
+      model: 'facebook/mms-tts-spa'
+    });
 
-  console.log('Respuesta de la API de Hugging Face: ', response);
+    console.log('Respuesta de la API de Hugging Face: ', response);
 
-  const buffer = await response.arrayBuffer();
-  const bufferNode = Buffer.from(buffer);
-  fs.writeFileSync('output.flac', bufferNode);
+    const buffer = await response.arrayBuffer();
+    const bufferNode = Buffer.from(buffer);
+    fs.writeFileSync('output.flac', bufferNode);
 
-  console.log('\nArchivo de audio guardado como output.flac');
+    console.log('\nArchivo de audio guardado como output.flac');
+  } catch (error) {
+    console.error('Error al generar el texto a voz:', error);
+  }
 }
 
 saveTextToSpeech();
